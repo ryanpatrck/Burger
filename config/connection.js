1d9f518967e4145b73102d5b919f0a9c866e559d
Ryan <Admin@MacBook-Pro.local>
2018-05-16 19:17:02 -0400

    burger

--- config/connection.js
+++ config/connection.js
@@ -5,4 +5,10 @@ const mysql = require('mysql')
           password:'root',
           database:'burgers_db'
       })
-      
+
+connection.connect((err) =>{
+    if (err) throw err
+    console.log("connected as id: "+connection.threadid)
+})
+
+module.exports = connections
