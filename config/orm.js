0b2d37cef699d9fe78c751da366dc0b74365f94b
Ryan <Admin@MacBook-Pro.local>
2018-05-16 19:51:58 -0400

    burger

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,9 @@
 const connection = require('./connection.js')
+      orm = {
+          all: (tableInput, cb) => {
+               connection.query('SELECT * FROM '+tableInput+';', (err, result) => {
+                if(err) throw err;
+                cb(result)
+               })
+          }
+      }
