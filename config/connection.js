5db11fab786de81807f3621c08dee03a4680e80f
Ryan <Admin@MacBook-Pro.local>
2018-05-19 11:06:25 -0400

    burgers

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,16 @@
-const mysql = require('mysql')
-      connection = mysql.createConnection({
-          host:'localhost',
-          user:'root',
-          password:'root',
-          database:'burgers_db'
-      })
+var   mysql = require('mysql');
+var   connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}   else {
+    connection = mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'root',
+        database:'burgers_db'
+    })
+}
 
 connection.connect((err) =>{
     if (err) throw err
